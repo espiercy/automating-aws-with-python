c8a689ca1138912229083f61626f245bf9e9124b
Evan <evanpiercy@gmail.com>
2020-04-11 15:23:33 -0700

    added domain manager

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -16,9 +16,13 @@ import boto3
 import click
 
 from bucket import BucketManager
+from domain import DomainManager
+import util
+
 
 session = None
 bucket_manager = None
+domain_manager = None
 
 session = boto3.Session(profile_name='pythonAutomation')
 bucket_manager = BucketManager(session)
@@ -30,13 +34,15 @@ bucket_manager = BucketManager(session)
               help="Use a given AWS profile.")
 def cli(profile):
     """Webotron deploys websites to AWS"""
-    global session, bucket_manager
+    global session, bucket_manager, domain_manager
+
     session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
 
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
+    domain_manager = DomainManager(session)
 
 
 @cli.command('list-buckets')
@@ -52,6 +58,7 @@ def list_bucket_objects(bucket):
     """List objects in an s3 bucket"""
     for obj in bucket_manager.all_objects(bucket):
         print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
@@ -71,6 +78,19 @@ def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET"""
     bucket_manager.sync(pathname, bucket)
     print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
+
+
+@cli.command('setup-domain')
+@click.argument('domain')
+def setup_domain(domain):
+    """Configure DOMAIN to point to BUCKET."""
+    bucket = bucket_manager.get_bucket(domain)
+    zone = domain_manager.find_hosted_zone(domain) or domain_manager.create_hosted_zone(domain)
+
+    endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
+    a_record = domain_manager.create_s3_domain_record(zone, domain, endpoint)
+    print("Domain configured: http://{}".format(domain))
+    print(a_record)
 
 
 if __name__ == '__main__':
